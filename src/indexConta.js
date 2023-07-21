a2db4ac6b539e496f6979804a5f1d123adfbbec1
Mauricio Jackson <mauricioj.fragoso@msn.com>
2023-07-20 23:10:49 -0300

    orientacao revisao em casa

--- src/indexConta.js
+++ src/indexConta.js
@@ -1,18 +1,18 @@
+import { ContaCorrente } from './modules/conta-corrente.js';
 import { conta } from  './modules/conta.js';
 
 //instanciando objeto conta.
 const contaTiago = new conta(`123`, `12345`, `Thiago`);
-if (contaTiago.depositar(5000)){
-    console.log(`Deposito realizado com sucesso`);
-} else{
-    console.log(`Deposito não realizado com sucesso`);
-}
+contaTiago.depositar(1000);
+   
 
-const contaMarcos = new conta(`123`, `12345`, `Thiago`);
+const contaMarcos = new ContaCorrente(`123`, `123456`, `Marcos`);
 contaMarcos.depositar(1000);
 
+//quando tornarmos os atributos privados "#" estamos fazendo uma boa prática pois não da
+//liberdado para o programador alterar as infos de fora da classe.
+//contaMarcos.#saldo = 20000;
+
+contaMarcos.transferir(contaTiago, 100);
 console.log(contaTiago);
 console.log(contaMarcos);
-
-
-
--- src/modules/conta.js
+++ src/modules/conta.js
@@ -1,41 +1,82 @@
 //classe é para criar modelos, aqui no caso de conta bancaria.
 
 export class conta {
-    
+
     //definindo atributos, quando usamos os atributos no construtor nao precisa declarar osatributos em outro lugar
     /*saldo;
     agencia;
     numero;
     titular;*/
 
+    #agencia;
+    #numero;
+    #titular;
+    #saldo = 0;
+
     //definindo o construtor
-    constructor(agencia, numero, titular){
-        this.agencia = agencia;
-        this.numero = numero;
-        this.titular = titular;
-        this.saldo = 0;
+    constructor(agencia, numero, titular) {
+        this.#agencia = agencia;
+        this.#numero = numero;
+        this.#titular = titular;
     }
 
     //definindo metodos
-    depositar(valor){
-        if (valor > 0) {
-        this.saldo += valor; 
-        }
+    get agencia() {
+        return this.#agencia;
     }
 
-    sacar(valor){
-        podeSacar = valor > 0 && valor <= this.saldo;
-        if (podeSacar) {
-        this.saldo -= valor; 
+    set agencia(novaAgencia) {
+        this.#agencia = novaAgencia;
+    }
+
+    get numero() {
+        return this.#numero;
+    }
+
+    set numero(novoNumero) {
+        this.#numero = novoNumero;
+    }
+
+    get titular() {
+        return this.#titular;
+    }
+
+    set titular(novoTitular) {
+        if (novoTitular === ``) {
+            throw new Error(`Titular não pode ser em branco`);
         }
+        this.#titular = novoTitular;
+    }
+
+    get saldo() {
+        return this.#saldo;
+    }
+
+
+    depositar(valor) {
+        const podeDepositar = valor > 0;
+        if (podeDepositar) {
+            this.#saldo += valor;
+            return true;
+        }
+        return false;
+    }
+
+    sacar(valor) {
+        const podeSacar = valor > 0 && valor <= this.#saldo;
+        if (podeSacar) {
+            this.#saldo -= valor;
+            return true;
+        }
+        return false;
     }
 
     transferir(contaDestino, valor) {
-        podetransferir = valor > 0 && valor <= this.saldo;
-        if(podetransferir) {
-            this.salda -= valor;
-            contaDestino.saldo += valor;
+        const podeTransferir = this.sacar(valor);
+        if (podeTransferir) {
+            return contaDestino.depositar(valor);
         }
+        return false;
     }
 
 }
